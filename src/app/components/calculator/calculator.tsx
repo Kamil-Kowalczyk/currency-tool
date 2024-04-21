66b6b857e117acb1b8281a0cc90fe89bc8877e31
Kamil-Kowalczyk <kamil2005aaa@gmail.com>
2024-04-21 20:58:56 +0200

    completed calculator

--- src/app/components/calculator/calculator.tsx
+++ src/app/components/calculator/calculator.tsx
@@ -3,63 +3,63 @@ import styles from './calculator.module.scss';
 import { useState } from "react";
 import { useCurrencyContext } from '../../contexts/currency-data-context/currency-data-context';
 import AmountInput from './amount-input/amount-input';
-import CurrencySelect from './currency-select/currency-select';
+import CurrencySelect, { SelectOption } from './currency-select/currency-select';
 import { Currency } from 'src/app/contexts/currency-data-context/models/currency-model';
+import { SingleValue } from 'react-select';
 
 export interface CalculatorFormProps {
     currencyTable: Currency[]
 }
 
-function CalculatorForm({currencyTable} : CalculatorFormProps) {
-
-    const [amount, setAmount] = useState<number>(0)
+function CalculatorForm({ currencyTable } : CalculatorFormProps) {
+    const [amount, setAmount] = useState<string>("1")
     const [hasFirstChanged, setHasFirtsChanged] = useState<boolean>(false)
     const [firstCur, setFirstCur] = useState<number>(0)
     const [secondCur, setSecondCur] = useState<number>(0)
 
-    let firstAmount: number
-    let secondAmount: number
-    let rate: number = calculateRate()
-    if (hasFirstChanged) {
-        firstAmount = amount
-        secondAmount = amount * rate
-    } else {
-        secondAmount = amount
-        firstAmount = amount / rate
-    }
-
-    function calculateRate(): number {
+    const calculateRate = () => {
         let calculatedRate = currencyTable[firstCur].rate / currencyTable[secondCur].rate
         return calculatedRate
     }
 
-    function handleAmountChange(event: React.ChangeEvent<HTMLInputElement>, firstChanged: boolean) {
-        event.preventDefault()
-        if (event.target.value == "")
-            setAmount(0)
-        else
-            setAmount(parseInt(event.target.value))
+    const handleAmountChange = (value: string, firstChanged: boolean) => {
+        setAmount(value)
         setHasFirtsChanged(firstChanged)
     }
+
+    let firstAmount: string
+    let secondAmount: string
+    let rate: number = calculateRate()
+    
+    rate = calculateRate()
+    if (firstCur == secondCur) {
+        firstAmount = amount
+        secondAmount = amount
+    } else if (hasFirstChanged) {
+        firstAmount = amount
+        secondAmount = (parseFloat(amount) * rate).toFixed(2)
+    } else {
+        secondAmount = amount
+        firstAmount = (parseFloat(amount) / rate).toFixed(2)
+    }
+    
 
     return (
         <form>
             <div className="row">
-                <div className="col text-center">
-                    <CurrencySelect data={currencyTable} name={'firstCurrency'} 
-                        onChange={e => {
-                                setFirstCur(parseInt(e.target.value))
-                                setHasFirtsChanged(true)
-                            }
-                        }/>
+                <div className="col-6 text-center">
+                    <CurrencySelect 
+                        data={currencyTable} 
+                        value={firstCur}
+                        onChange={option => setFirstCur(option ? option.value : 0)}
+                    />
                 </div>
-                <div className="col text-center">
-                    <CurrencySelect data={currencyTable} name={'secondCurrency'} 
-                        onChange={e => {
-                                setSecondCur(parseInt(e.target.value))
-                                setHasFirtsChanged(false)
-                            }
-                        }/>
+                <div className="col-6 text-center">
+                    <CurrencySelect 
+                        data={currencyTable} 
+                        value={secondCur} 
+                        onChange={option => setSecondCur(option ? option.value : 0)}
+                    />
                 </div>
             </div>
             <div className="row">
@@ -69,12 +69,18 @@ function CalculatorForm({currencyTable} : CalculatorFormProps) {
             </div>
             <div className="row">
                 <div className="col text-center">
-                    <AmountInput name={'firstAmount'} value={firstAmount} 
-                        onChange={e => handleAmountChange(e, true)}/>
+                    <AmountInput 
+                        value={firstAmount} 
+                        onChange={value => handleAmountChange(value, true)}
+                        min={0}
+                    />
                 </div>
                 <div className="col text-center">
-                    <AmountInput name={'secondAmount'} value={secondAmount} 
-                        onChange={e => handleAmountChange(e, false)}/>
+                    <AmountInput 
+                        value={secondAmount} 
+                        onChange={value => handleAmountChange(value, false)}
+                        min={0}
+                    />
                 </div>
             </div>
         </form>
